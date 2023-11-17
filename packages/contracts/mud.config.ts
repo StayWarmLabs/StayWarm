2dac2fc73708ca7c8c7512544b1b540e33c5209a
Noy <59097122+noyyyy@users.noreply.github.com>
2023-11-18 00:33:13 +0300

    feat: add schema

--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -2,9 +2,63 @@ import { mudConfig } from "@latticexyz/world/register";
 
 export default mudConfig({
   tables: {
-    Counter: {
+    Config: {
       keySchema: {},
-      valueSchema: "uint32",
+      valueSchema: {
+        gameStartWaitingTime: "uint32",
+        roundTimeLength: "uint32",
+        voteTimeLength: "uint32",
+        burnAmountPerRound: "uint256",
+      },
     },
+    Game: {
+      keySchema: {},
+      valueSchema: {
+        startTime: "uint32",
+        ethTotalAmount: "uint256",
+        currentRound: "uint32",
+        ExecuteFuncSig: "bytes32",
+        ExecuteArgs: "bytes32",
+      },
+    },
+    //
+    Player: {
+      keySchema: { key: "address" },
+      valueSchema: {
+        status: "PlayerStatus",
+        lastCheckedTime: "uint32",
+        ftBalance: "uint256",
+        burnedAmount: "uint256",
+      },
+    },
+    Proposal: {
+      keySchema: { id: "bytes32" },
+      valueSchema: {
+        proposer: "address",
+        startTime: "uint32",
+        for: "uint32",
+        against: "uint32",
+        executed: "bool",
+        register: "bytes32[]",
+        unregister: "bytes32[]",
+        uri: "string", // ipfs://Qmxxx
+      },
+    },
+    Register: {
+      keySchema: { id: "bytes32" },
+      valueSchema: {
+        resourcesId: "bytes32",
+        implAddr: "address",
+      },
+    },
+    Unregister: {
+      keySchema: { id: "bytes32" },
+      valueSchema: {
+        resourcesId: "bytes32",
+      },
+    },
+  },
+  enums: {
+    PlayerStatus: ["UNINITIATED", "ALIVE", "DEAD"],
   },
 });
---
+++ packages/contracts/src/systems/ExecuteSystem.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.21;
+
+import {System} from "@latticexyz/world/src/System.sol";
+import {Counter} from "../codegen/index.sol";
+
+contract ExecuteSystem is System {
+    function run() public returns (uint32) {
+        // if (if one round pass) {
+        //     execute sth
+        // }
+
+        // if (if 14 days pass) {
+        //     execute sth
+        // }
+    }
+}
