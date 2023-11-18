d5c469eff8faae464016485c5fd5ffd7f0c3bcab
SYORA17 <112358132134.fibon@gmail.com>
2023-11-18 09:45:11 +0300

    deleted some systems and test

--- packages/contracts/src/systems/UpgradeSystem.sol
+++ packages/contracts/src/systems/UpgradeSystem.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.21;
 
 import {System} from "@latticexyz/world/src/System.sol";
-import {Counter} from "../codegen/index.sol";
+// import {Counter} from "../codegen/index.sol";
 import "@latticexyz/world/src/modules/core/implementations/WorldRegistrationSystem.sol";
 import {ResourceId, ResourceIdInstance} from "@latticexyz/store/src/ResourceId.sol";
 import {WorldContextConsumer, WORLD_CONTEXT_CONSUMER_INTERFACE_ID} from "@latticexyz/world/src/WorldContext.sol";
