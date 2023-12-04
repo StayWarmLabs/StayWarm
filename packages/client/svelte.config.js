8b9e784335eb6fd368ef3e372d0dac04af61ed9d
Noy <59097122+noyyyy@users.noreply.github.com>
2023-12-04 17:59:13 +0800

    build: add vercel adapter

--- packages/client/svelte.config.js
+++ packages/client/svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import vercelAdapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +9,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: vercelAdapter()
 	}
 };
 
